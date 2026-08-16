30a35a4507151f5ade2ecdf7e7417caf15f4b8dc
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2026-08-16 17:55:47 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        String output = "";
+
+        for(String str : strs){
+            output += str.length() + "_" + str;
+        }
+        return output;
+    }
+
+    public List<String> decode(String str) {
+
+        List<String> output = new ArrayList<>();
+
+        for(int i = 0; i < str.length(); i++){
+            String l = "";
+            while(str.charAt(i) != '_'){
+                l += str.charAt(i++);
+            }
+            i++;
+            int len = Integer.valueOf(l);
+            String out = str.substring(i, i+len);
+            output.add(out);
+            i += len-1;
+        }
+
+        return output;
+    }
+}
