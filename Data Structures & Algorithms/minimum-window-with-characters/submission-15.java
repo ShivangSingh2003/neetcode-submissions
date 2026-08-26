164bc0dc0e829cd8bc2d0e276f7768bde1838497
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2026-08-26 07:44:44 +0530

    Add: minimum-window-with-characters - submission-15

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-15.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int len1 = s.length();
+        int len2 = t.length();
+
+        if(len1<len2)
+            return "";
+        
+        int[] sArr = new int[123];
+        int[] tArr = new int[123];
+        int required = 0;
+
+        for(int i = 0; i < len2; i++){
+            char ch = t.charAt(i);
+            tArr[(int)ch] += 1;
+            if (tArr[(int)ch] == 1) required++;
+        }
+        int left = 0;
+        int right = 0;
+        int minWindow = 100000;
+        boolean isValid = true;
+        String ans = "";
+        int formed = 0;
+
+        while(right < len1){
+            char ch = s.charAt(right);
+            sArr[(int)ch] ++;
+            if(sArr[(int)ch] == tArr[(int)ch]) formed++;
+            while(formed == required){
+                char c = s.charAt(left);
+                if(sArr[(int)c] == tArr[(int)c]){
+                    if(right-left+1 < minWindow){
+                        minWindow = right-left+1;
+                        ans = s.substring(left, right+1);
+                    }
+                    formed--;
+                }
+                sArr[(int)c] --;
+                left++;
+            }
+            right++;
+        }
+        return ans;
+    }
+}
