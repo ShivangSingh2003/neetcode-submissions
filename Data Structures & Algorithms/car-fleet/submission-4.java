bad7f478cd482a54df4f4861639f093fb73e2452
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2026-09-02 22:03:22 +0530

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        List<int[]> lst = new ArrayList<>();
+
+        for(int i = 0; i < speed.length; i++){
+            int[] l = new int[2];
+            l[0] = position[i];
+            l[1] = speed[i];
+            lst.add(l);
+        }
+
+        lst.sort((a,b) -> a[0] - b[0]);
+
+        double lastFleetTime = 0;
+        int fleets = 0;
+        for(int i = lst.size()-1; i>=0; i--){
+            int[] arr = lst.get(i);
+            int pos = arr[0];
+            int s = arr[1];
+
+            double time = (double)(target-pos)/s;
+            if(time>lastFleetTime){
+                fleets++;
+                lastFleetTime = time;
+            }
+        }
+
+        return fleets;
+    }
+}
