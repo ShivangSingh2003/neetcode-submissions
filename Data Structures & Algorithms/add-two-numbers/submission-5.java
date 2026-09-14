6826d024c9d47491deb90eadfe6a88167a300187
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2026-09-14 19:02:12 +0530

    Add: add-two-numbers - submission-5

---
+++ Data Structures & Algorithms/add-two-numbers/submission-5.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+        int carry = 0;
+        while(l1 != null && l2 != null){
+            int sum = l1.val + l2.val + carry;
+            ListNode temp = null;
+            if(sum > 9){
+                carry = sum/10;
+                temp = new ListNode(sum%10);
+            }
+            else{
+                carry = 0;
+                temp = new ListNode(sum);
+            }
+            curr.next = temp;
+            curr = temp;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while(l1 != null){
+            int sum = l1.val + carry;
+            ListNode temp = null;
+            if(sum > 9){
+                carry = sum/10;
+                temp = new ListNode(sum%10);
+            }
+            else{
+                carry = 0;
+                temp = new ListNode(sum);
+            }
+            curr.next = temp;
+            curr = temp;
+            l1 = l1.next;
+        }
+        while(l2 != null){
+            int sum = l2.val + carry;
+            ListNode temp = null;
+            if(sum > 9){
+                carry = sum/10;
+                temp = new ListNode(sum%10);
+            }
+            else{
+                carry = 0;
+                temp = new ListNode(sum);
+            }
+            curr.next = temp;
+            curr = temp;
+            l2 = l2.next;
+        }
+        if(carry != 0){
+            ListNode temp = new ListNode(carry);
+            curr.next = temp;
+            curr = temp;
+        }
+        curr.next = null;
+
+        return dummy.next;
+    } 
+}
